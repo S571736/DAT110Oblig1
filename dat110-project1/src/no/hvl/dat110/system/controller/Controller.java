144a4559128ad0a426590b964026628d365055f4
S571736 <Sondre.Gjesdal@gmail.com>
2019-02-05 10:05:03 +0100

    DONE

--- dat110-project1/src/no/hvl/dat110/system/controller/Controller.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Controller.java
@@ -47,7 +47,7 @@ public class Controller  {
 		displayclient.register(stopdisplay);
 		sensorclient.register(stopsensor);
 
-		for (int i = 0; i<10; i++){
+		for (int i = 0; i < N; i++){
 			display.write("Temperatur: " + sensor.read());
 			try{
 				Thread.sleep(1000);
